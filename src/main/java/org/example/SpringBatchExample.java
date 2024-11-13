77994c11b38798f0b8bbd7bc3e44773986a64fe2
NathanQuellec <71017057+NathanQuellec@users.noreply.github.com>
2024-11-13 16:05:54 +0100

    migrate from xml config to java config

--- src/main/java/org/example/SpringBatchExample.java
+++ src/main/java/org/example/SpringBatchExample.java
@@ -1,16 +1,19 @@
 package org.example;
 
+import org.example.config.PersonJobConfig;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringBatchExample {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("batch-job.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("batch-job.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(PersonJobConfig.class);
 
         JobLauncher jobLauncher = context.getBean(JobLauncher.class);
         Job job = context.getBean("personJob", Job.class);
---
+++ src/main/java/org/example/config/PersonJobConfig.java
@@ -0,0 +1,90 @@
+package org.example.config;
+
+import org.example.Person;
+import org.example.PersonItemProcessor;
+import org.example.PersonItemWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableBatchProcessing
+public class PersonJobConfig {
+
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public PersonJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public FlatFileItemReader<Person> personReader() {
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("input.csv"));
+
+        // Configure the line mapper
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+
+        // Set up the tokenizer for parsing lines
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames(new String[]{"firstName", "lastName"});
+
+        // Set up the field set mapper to map fields to the Person class
+        BeanWrapperFieldSetMapper<Person> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Person.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        reader.setLineMapper(lineMapper);
+
+        return reader;
+
+    }
+
+    @Bean
+    public PersonItemProcessor personItemProcessor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public PersonItemWriter personItemWriter() {
+        return new PersonItemWriter();
+    }
+
+    @Bean
+    public Job personJob() {
+        return null;
+    }
+
+    @Bean
+    public Step personStep(FlatFileItemReader<Person> personReader, PersonItemProcessor personItemProcessor, PersonItemWriter personItemWriter) {
+        return stepBuilderFactory.get("personStep")
+                .<Person, Person>chunk(1)
+                .reader(personReader)
+                .processor(personItemProcessor)
+                .writer(personItemWriter)
+                .build();
+    }
+
+    @Bean
+    public Job personJob(Step personStep) {
+        return jobBuilderFactory.get("personJob")
+                .start(personStep)
+                .build();
+    }
+}
